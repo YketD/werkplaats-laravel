10eaf77f47dffc66872fd510855529f74d8e2c6e
Yke ten Donkelaar <y.tendonkelaar@code14.nl>
2020-03-04 20:42:42 +0100

    added reservation save

---
+++ app/Entities/Reservation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Reservation
+ *
+ * @package App\Entities
+ */
+class Reservation extends Model
+{
+
+}
--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Entities\Reservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class OrderController extends Controller
 {
     public function sendOrder(Request $request)
     {
+        $reservation = new Reservation();
 
+        $reservation->email = Arr::get($request, 'email', 'Not provided');
+        $reservation->phone = Arr::get($request, 'phone', 'Not provided');
+        $reservation->fullname = Arr::get($request, 'fullName', 'Not provided');
+        $reservation->plan = Arr::get($request, 'plan', 'Not provided') . " voor " . Arr::get($request, 'time', 'Not provided');
+        $reservation->save();
 
         return response()->json($request->all());
     }
