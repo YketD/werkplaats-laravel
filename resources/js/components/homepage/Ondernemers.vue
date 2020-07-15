cb7347a5f2773e0f92fd8012c73d3887441985ad
Yke ten Donkelaar <y.tendonkelaar@code14.nl>
2020-07-16 00:13:44 +0200

    fix

--- resources/js/components/homepage/Ondernemers.vue
+++ resources/js/components/homepage/Ondernemers.vue
@@ -2,7 +2,7 @@
     <div class="green-content-box">
         <div class="green-content">
             <div class="grid">
-                <div class="grid-item">
+                <div class="grid-item blank light-green">
                     <a target="_blank" href="https://www.judithdepotter.nl" class="image-container">
                         <img src="/storage/img/clients/2.png" />
                     </a>
@@ -11,6 +11,26 @@
                     <a target="_blank" href="https://www.communitycompany.nl" class="image-container image-container-2">
                         <img src="/storage/img/clients/1.jpg" />
                     </a>
+                </div>
+
+                <div class="grid-item blank light-grey">
+                </div>
+                <div class="grid-item blank white">
+                    <a target="_blank" href="https://www.detwentsezorgcentra.nl/"
+                       class="image-container image-container-3">
+                        <img src="/storage/img/clients/4.jpg" />
+                    </a>
+
+                </div>
+                <div class="grid-item blank grey">
+                </div>
+                <div class="grid-item blank green">
+                </div>
+                <div class="grid-item blank light-green">
+                </div>
+                <div class="grid-item blank green">
+                </div>
+                <div class="grid-item blank green">
                 </div>
             </div>
         </div>
@@ -37,50 +57,77 @@
     .grid {
         display               : grid;
         height                : 100%;
-        grid-template-columns : repeat(2, 20vw);
-        grid-template-rows    : repeat(1, 20vw);
-
+        grid-template-columns : repeat(4, 1fr);
+        grid-template-rows    : repeat(2, 1fr);
+        grid-gap              : 50px;
+        top                   : 50px;
         @media(max-width : 992px) {
-            grid-template-columns : repeat(1, 90vw);
-            grid-template-rows    : repeat(2, 50vw);
-            grid-gap              : 0px;
+            grid-template-columns : repeat(3, 1fr);
+            grid-template-rows    : repeat(2, 1fr);
+        }
+        @media(max-width : 500px) {
+            grid-template-columns : repeat(2, 124px);
+            grid-template-rows    : repeat(4, 124px);
             top: 0;
         }
 
-        grid-gap              : 50px;
-        top                   : 50px;
 
         .grid-item {
             position : relative;
 
+            &.blank {
+                background-color : grey;
+                border-radius    : 100%;
+
+                &.light-green {
+                    background-color : #bccfbf;
+                    top              : 20px;
+                    left             : 15px;
+                }
+
+                &.green {
+                    background : #ddffdd;
+                    top        : 10px;
+                    left       : -15px;
+                }
+
+                &.light-grey {
+                    background : #a1a1a1
+                }
+
+                &.white {
+                    background: white;
+                }
+            }
+            border-radius: 100%;
+
+            &:hover {
+                transform  : scale(1.02);
+                box-shadow : 5px 9px 25px 0px rgba(0, 0, 0, 0.25);
+            }
+            transition    : all .2s;
+
             .image-container {
                 /*position        : absolute;*/
-                top           : 1vw;
-                left          : 2vw;
+
 
                 &.image-container-2 {
-                    top  : 5vw;
-                    left : 5vw;
+
                 }
 
-                height        : 15vw;
-                width         : 15vw;
-                @media(max-width : 992px) {
-                    height : 50vw;
-                    width  : 50vw;
-                    left   : 25vw;
+                &image-container-3 {
+
                 }
+
+                height        : 100%;
+                width         : 100%;
+
                 border-radius : 100%;
-                background    : #dddddd;
                 display       : flex;
                 align-items   : center;
                 overflow      : hidden;
-                transition    : all .2s;
 
-                &:hover {
-                    transform  : scale(1.02);
-                    box-shadow : 5px 9px 25px 0px rgba(0, 0, 0, 0.25);
-                }
+
 
                 img {
                     width      : 100%;
@@ -94,6 +141,8 @@
         font-size   : 1.5vw;
         line-height : 2.5vw;
         max-height  : 10vw;
+        padding: 24px;
+        max-width: calc(100% - 48px);
 
 
         @media ('max-width : 500px') {
