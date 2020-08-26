5c39960cdf751c83972d6ed2e32eee685de8bac5
Yke ten Donkelaar <y.tendonkelaar@code14.nl>
2020-08-26 11:13:07 +0200

    fix meta description and title

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,10 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="icon" href="<%= BASE_URL %>favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,400i,700&display=swap" rel="stylesheet">
     <link href="{{mix('css/app.css')}}" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <script src="https://unpkg.com/vue"></script>
     <script src="https://unpkg.com/prismic-vue"></script>
     <script>
@@ -16,7 +22,9 @@
         };
     </script>
     <script src="https://static.cdn.prismic.io/prismic.min.js"></script>
-    <title>werkplaats75c</title>
+    <title>Werkplaats75C - Flexibele werkpleken</title>
+    <meta name="description"
+          content="Werkplaats75C biedt fijne Flexibele werkplekken, met vergader- en workshopruimte, flexibele openingstijden voor zzp-ers en co-workers.">
 </head>
 <body>
     <noscript>
