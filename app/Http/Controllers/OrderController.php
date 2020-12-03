e8aec2bb485964bc5a19eb5b3121e699d82e1b91
Yke ten Donkelaar <y.tendonkelaar@code14.nl>
2020-12-03 20:27:06 +0100

    fix email and add a test mail as validation

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -79,9 +79,12 @@ class OrderController extends Controller
 
     public function activityAdmission(Request $request)
     {
-        Log::info('teste');
         Mail::send('mail.holly', ['email' => $request->email], function ($message) {
-            $message->to('yketd@hotmail.com')->subject('Nieuwe aanmelding!')->from('info@werkplaats75c.nl');
+            $message->to('hollykleinoonkphotography@gmail.com')->subject('Nieuwe aanmelding!')->from('info@werkplaats75c.nl');
+        });
+
+        Mail::send('mail.holly', ['email' => $request->email], function ($message) {
+            $message->to('yketd@hotmails.com')->subject('Nieuwe aanmelding! - test mail als verzekering')->from('info@werkplaats75c.nl');
         });
     }
 
